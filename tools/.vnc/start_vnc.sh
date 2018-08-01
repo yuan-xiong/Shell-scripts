3eeb039c96853c240f4d83a37c1eaa15cf0a6185
abby0100 <yuan.xiong@intel.com>
2018-08-01 21:55:09 +0800

    add for vnc server scipts.

---
+++ tools/.vnc/start_vnc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# passwd: 12345678
+# vncserver :4 -geometry 1600x800 -alwaysshared -depth 24
+vncserver :4 -geometry 1800x960 -alwaysshared -depth 24
+#vncserver :4 -geometry 1920x1080 -alwaysshared -depth 24
